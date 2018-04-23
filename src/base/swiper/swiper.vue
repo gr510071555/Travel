f0bcde5e6d496d10cf8c3b27d67ff67b51c57833
gr <510071555@qq.com>
2018-04-23 13:35:52 +0800

    change

---
+++ src/base/swiper/swiper.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-image" :src="item.imgUrl">
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '0001',
+          imgUrl: 'https://pics.lvjs.com.cn/pics/super/2018/04/1523356038_29607.jpg'
+        },
+        {
+          id: '0002',
+          imgUrl: 'https://pics.lvjs.com.cn/pics/super/2018/04/1524108144_69898.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    width: 100%
+    height: 0
+    overflow: hidden
+    padding-bottom: 24.15%
+    .swiper-image
+      width: 100%
+</style>
--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <m-header></m-header>
+    <home-swiper></home-swiper>
   </div>
 </template>
 
 <script>
 import MHeader from 'components/m-header/m-header'
+import HomeSwiper from 'base/swiper/swiper'
 export default {
   components: {
-    MHeader
+    MHeader,
+    HomeSwiper
   }
 }
 </script>
