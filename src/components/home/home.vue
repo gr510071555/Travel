3850c1dcc7cc2ad3db53990edd2e626077ab36e7
gr <510071555@qq.com>
2018-04-23 22:23:35 +0800

    add icons

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -2,16 +2,19 @@
   <div>
     <m-header></m-header>
     <home-swiper></home-swiper>
+    <icons></icons>
   </div>
 </template>
 
 <script>
 import MHeader from 'components/m-header/m-header'
 import HomeSwiper from 'base/swiper/swiper'
+import Icons from 'components/icons/icons'
 export default {
   components: {
     MHeader,
-    HomeSwiper
+    HomeSwiper,
+    Icons
   }
 }
 </script>
---
+++ src/components/icons/icons.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="icons">
+    <swiper>
+      <swiper-slide v-for="(page, index) of pages" :key="index">
+        <div class="icon" v-for="item of page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.imgUrl">
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      iconList: [
+        {
+          id: '0001',
+          imgUrl: 'https://pics.lvjs.com.cn/pics/super/2018/04/1523426245_96136.jpg',
+          desc: '热门景点'
+        },
+        {
+          id: '0002',
+          imgUrl: 'https://pics.lvjs.com.cn/pics/super/2018/04/1523426245_96136.jpg',
+          desc: '热门景点'
+        },
+        {
+          id: '0003',
+          imgUrl: 'https://pics.lvjs.com.cn/pics/super/2018/04/1523426245_96136.jpg',
+          desc: '热门景点'
+        },
+        {
+          id: '0004',
+          imgUrl: 'https://pics.lvjs.com.cn/pics/super/2018/04/1523426245_96136.jpg',
+          desc: '热门景点'
+        },
+        {
+          id: '0005',
+          imgUrl: 'https://pics.lvjs.com.cn/pics/super/2018/04/1523426245_96136.jpg',
+          desc: '滑雪季'
+        },
+        {
+          id: '0006',
+          imgUrl: 'https://pics.lvjs.com.cn/pics/super/2018/04/1523426245_96136.jpg',
+          desc: '泡温泉'
+        },
+        {
+          id: '0007',
+          imgUrl: 'https://pics.lvjs.com.cn/pics/super/2018/04/1523426245_96136.jpg',
+          desc: '动植园'
+        },
+        {
+          id: '0008',
+          imgUrl: 'https://pics.lvjs.com.cn/pics/super/2018/04/1523426245_96136.jpg',
+          desc: '热门景点'
+        },
+        {
+          id: '0009',
+          imgUrl: 'https://pics.lvjs.com.cn/pics/super/2018/04/1523426245_96136.jpg',
+          desc: '热门景点'
+        }
+      ]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~assets/styles/variable.styl"
+  @import "~assets/styles/mixins.styl"
+  .icons >>> .swiper-container
+    height: 0
+    padding-bottom: 50%
+  .icon
+      position: relative
+      float: left
+      width: 25%
+      height: 0
+      padding-bottom: 25%
+      overflow: hidden
+      .icon-img
+        position: absolute
+        padding: .1rem
+        top: 0
+        left: 0
+        right: 0
+        bottom: .44rem
+        box-sizing: border-box
+        .icon-img-content
+          display: block
+          margin: 0 auto
+          height: 100%
+      .icon-desc
+        position: absolute
+        left: 0
+        right: 0
+        bottom: 0
+        height: .44rem
+        line-height: .44rem
+        text-align: center
+        color: $darkTextColor
+        ellipsis()
+</style>
