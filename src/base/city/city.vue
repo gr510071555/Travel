063932451bcb90e9a4052a07818384774ba35c93
gr <510071555@qq.com>
2018-04-24 14:38:21 +0800

    city router

---
+++ src/base/city/city.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <city-list></city-list>
+  </div>
+</template>
+
+<script>
+import CityList from 'components/cityList/cityList'
+export default{
+  components: {
+    CityList
+  }
+}
+</script>
+
+<style lang="stylus" scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from 'components/home/home'
+import City from 'base/city/city'
 
 Vue.use(Router)
 
@@ -13,6 +14,10 @@ export default new Router({
     {
       path: '/home',
       component: Home
+    },
+    {
+      path: '/city',
+      component: City
     }
   ]
 })
