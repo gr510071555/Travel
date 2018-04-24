9cfcb325f02755e3a3f4bd3af6ea412b1da7fad6
gr <510071555@qq.com>
2018-04-24 15:33:47 +0800

    search input

--- src/base/city/city.vue
+++ src/base/city/city.vue
@@ -1,16 +1,21 @@
 <template>
   <div>
     <city-list></city-list>
+    <city-search></city-search>
   </div>
 </template>
 
 <script>
 import CityList from 'components/cityList/cityList'
+import CitySearch from 'base/search/search'
 export default{
   components: {
-    CityList
+    CityList,
+    CitySearch
   }
 }
 </script>
 
-<style lang="stylus" scoped></style>
+<style lang="stylus" scoped>
+
+</style>
---
+++ src/base/search/search.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="search">
+    <input class="search-input" type="text" :placeholder="placeholder">
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    placeholder: {
+      type: String,
+      default: '输入城市名或拼音'
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~assets/styles/variable.styl'
+  .search
+    height: .72rem
+    padding: 0 .1rem
+    background: $bgColor
+    .search-input
+      width: 100%
+      padding: 0 .1rem
+      height: .62rem
+      line-height: .62rem
+      text-align: center
+      border-radius: .06rem
+      color: #666
+      box-sizing: border-box
+</style>
