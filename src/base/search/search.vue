b69b3acde147bd04c2ef8022a912408c12b66f38
gr <510071555@qq.com>
2018-04-25 13:13:04 +0800

    city-search logic

--- src/base/search/search.vue
+++ src/base/search/search.vue
@@ -1,15 +1,62 @@
 <template>
-  <div class="search">
-    <input class="search-input" type="text" :placeholder="placeholder">
+  <div>
+    <div class="search">
+      <input v-model="keyword" class="search-input" type="text" :placeholder="placeholder">
+    </div>
+    <div class="search-content" ref="searchContent" v-show="keyword">
+      <ul>
+        <li class="search-item border-bottom" v-for="item of list" :key="item.id">{{item.name}}</li>
+        <li class="search-item border-bottom" v-show="hasNoData">没有找到匹配数据</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import BScroll from 'better-scroll'
 export default {
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
   props: {
     placeholder: {
       type: String,
       default: '输入城市名或拼音'
+    },
+    cities: Object
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
+  mounted () {
+    this.scroll = new BScroll(this.$refs.searchContent)
+  },
+  watch: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
     }
   }
 }
@@ -30,4 +77,18 @@ export default {
       border-radius: .06rem
       color: #666
       box-sizing: border-box
+  .search-content
+    position: absolute
+    top: 1.58rem
+    left: 0
+    right: 0
+    bottom: 0
+    overflow: hidden
+    background: #eee
+    z-index: 1
+    .search-item
+      line-height: .62rem
+      padding-left: .2rem
+      color: #666
+      background: #fff
 </style>
