703012231874cae9a75ba3c21feaf006a5491223
gr <510071555@qq.com>
2018-04-27 15:53:02 +0800

    header animation

--- src/components/detail/detail.vue
+++ src/components/detail/detail.vue
@@ -1,18 +1,23 @@
 <template>
   <div>
     <banner></banner>
+    <detail-header></detail-header>
+    <div class="content"></div>
   </div>
 </template>
 
 <script>
 import Banner from 'base/banner/banner'
+import DetailHeader from 'components/detail-header/detail-header'
 export default {
   components: {
-    Banner
+    Banner,
+    DetailHeader
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+  .content
+    height: 50rem
 </style>
