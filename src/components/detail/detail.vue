b4fc0d5d4e9e0cfd3110f1c731dfbfa9e631a522
gr <510071555@qq.com>
2018-04-27 15:15:31 +0800

    banner finish

---
+++ src/components/detail/detail.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <banner></banner>
+  </div>
+</template>
+
+<script>
+import Banner from 'base/banner/banner'
+export default {
+  components: {
+    Banner
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from 'components/home/home'
 import City from 'components/city/city'
+import Detail from 'components/detail/detail'
 
 Vue.use(Router)
 
@@ -18,6 +19,10 @@ export default new Router({
     {
       path: '/city',
       component: City
+    },
+    {
+      path: '/detail/:id',
+      component: Detail
     }
   ]
 })
